2fd3557473ab7e64b5c36cc0cad321b962a3e520
carrotchair <39000323+carrotchair@users.noreply.github.com>
2022-04-07 15:09:50 +0200

    Initial Commit

---
+++ src/test/java/com/example/homefinances/HomeFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homefinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
